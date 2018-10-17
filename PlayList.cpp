b8a5667f1b9e109ff74fbe5a11ded2d9da68dcac
Josh <joshcwinton@gmail.com>
2018-10-17 18:09:26 -0400

    comments added, formatting

--- PlayList.cpp
+++ PlayList.cpp
@@ -1,31 +1,44 @@
+// Title: PlayList.cpp
+// Author: Josh Winton
+// Purpopse: Function definitions for playlist class
+// Created: 10/17/18
+
 #include "PlayList.h"
 #include <iostream>
 
-PlayList::PlayList(){
+// default constructor, sets pointers and item_count_ to base values
+PlayList::PlayList() {
   head_ptr_ = nullptr;
   tail_ptr_ = nullptr;
   item_count_ = 0;
 }
 
-PlayList::PlayList(const Song& a_song){
-  add(a_song);
-}
+// PlayList::add takes care of starting a playlist with one item
+PlayList::PlayList(const Song &a_song) { add(a_song); }
 
-PlayList::~PlayList(){
-  unloop();
-}
+// destructor unloops to deallocate properly
+PlayList::~PlayList() { unloop(); }
 
-bool PlayList::add(const Song& new_song) {
-  if(contains(new_song)){
+// adds a new song to the end of the playlist if it isn't included already
+bool PlayList::add(const Song &new_song) {
+  // checks if song is already in playlist using contains from linked set
+  // false if song is already in playlist
+  if (contains(new_song)) {
     return false;
   } else {
-    Node<Song>* new_node_ptr = new Node<Song>;
-    new_node_ptr->setItem(new_song);
-    new_node_ptr->setNext(nullptr);
-    if(item_count_ == 0){
+    // note: I know there's another function that does this but I felt that
+    // this way was more readable and helped me debug more easily
+    Node<Song> *new_node_ptr =
+        new Node<Song>;              // allocates memory for a new node
+    new_node_ptr->setItem(new_song); // puts the new song in it
+    new_node_ptr->setNext(nullptr);  // marks node as the end of the chain
+    // case if the playlist is empty, added item is the first and last
+    if (item_count_ == 0) {
       head_ptr_ = new_node_ptr;
       tail_ptr_ = new_node_ptr;
     } else {
+      // if the playlist is not empty, point the last item to it, then make it
+      // the tail pointer
       tail_ptr_->setNext(new_node_ptr);
       tail_ptr_ = tail_ptr_->getNext();
     }
@@ -34,21 +47,38 @@ bool PlayList::add(const Song& new_song) {
   }
 }
 
-bool PlayList::remove(const Song& a_song) {
+// removes an item from the playlist
+bool PlayList::remove(const Song &a_song) {
+  // Node to be passed by reference to getPointerTo
+  // If getPointerTo can find the item, and its not the first, this points to
+  // the item before it
   Node<Song> *prev_ptr;
+  // stores the pointer to the node containing a_song, unless it's not in the
+  // playlist, in which case it's just nullptr
   Node<Song> *my_ptr = getPointerTo(a_song, prev_ptr);
-  if(my_ptr == nullptr){
-    return 0;
+  if (my_ptr == nullptr) {
+    return 0; // if the song isn't found, the function returns false
   } else {
-    if(head_ptr_ == my_ptr){
-      head_ptr_=my_ptr->getNext();
+    // if the song is the first item, head_ptr_ just get moves to the following
+    // item.
+    // note: this works even if it's the only item because then head_ptr_
+    // becomes nullptr
+    if (head_ptr_ == my_ptr) {
+      head_ptr_ = my_ptr->getNext();
       item_count_--;
       return true;
-    } else if(tail_ptr_ == my_ptr){
+      // if it's the last item, just set the previous item to point to nullptr
+      // instead
+      // note: this doesn't get checked if it's the only item, happens in the
+      // last
+      // if
+    } else if (tail_ptr_ == my_ptr) {
       prev_ptr->setNext(nullptr);
       tail_ptr_ = prev_ptr;
       item_count_--;
       return true;
+      // otherwise, just point, the previous item to the item that follows the
+      // removed song
     } else {
       prev_ptr->setNext(my_ptr->getNext());
       item_count_--;
@@ -58,49 +88,62 @@ bool PlayList::remove(const Song& a_song) {
   return 0;
 }
 
-void PlayList::loop(){
-  if(tail_ptr_ != nullptr){
+// points the last item back to the first
+void PlayList::loop() {
+  if (tail_ptr_ != nullptr) {
     tail_ptr_->setNext(head_ptr_);
   }
 }
 
-void PlayList::unloop(){
-  if(tail_ptr_ != nullptr){
+// does the opposite of loop by pointing tail_ptr_ back to nullptr
+void PlayList::unloop() {
+  if (tail_ptr_ != nullptr) {
     tail_ptr_->setNext(nullptr);
   }
 }
 
-void PlayList::displayPlayList(){
+// uses toVector from LinkedSet to print playlist info
+void PlayList::displayPlayList() {
   std::vector<Song> display_vector = toVector();
-  for(int i = 0; i < display_vector.size(); i++){
-    std::cout << "* Title: " << display_vector[i].getTitle() << "* Author: " << display_vector[i].getAuthor() << "* Album: " << display_vector[i].getAlbum() << " *" << std::endl;
+  for (int i = 0; i < display_vector.size(); i++) {
+    std::cout << "* Title: " << display_vector[i].getTitle()
+              << "* Author: " << display_vector[i].getAuthor()
+              << "* Album: " << display_vector[i].getAlbum() << " *"
+              << std::endl;
   }
   std::cout << "End of playlist" << '\n';
 }
 
- // Pointer to last node
-Node<Song>* PlayList::getPointerToLastNode() const {
+// Pointer to last node
+Node<Song> *PlayList::getPointerToLastNode() const {
+  // cylces through each node until it finds one that points to nullptr and
+  // returns a pointer to it
   Node<Song> *current = head_ptr_;
   Node<Song> *previous = nullptr;
   while (current != nullptr) {
-      previous = current;
-      current = current->getNext();
+    previous = current;
+    current = current->getNext();
   }
   return previous;
 }
 
-//
-Node<Song>* PlayList::getPointerTo(const Song& target, Node<Song>*& previous_ptr) const {
+// returns a pointer to a song matching target, and changes previous_ptr pointer
+// to reference the previous node
+Node<Song> *PlayList::getPointerTo(const Song &target,
+                                   Node<Song> *&previous_ptr) const {
   Song my_target = target;
   Node<Song> *current = head_ptr_;
   Node<Song> *previous = nullptr;
+  // cycles through each node until it finds one whose song matches target,
+  // which it returns
+  // if nothing is found, nullptr is returned
   while (current != nullptr) {
-      if(current->getItem() == my_target){
-        previous_ptr = previous;
-        return current;
-      }
-      previous = current;
-      current = current->getNext();
+    if (current->getItem() == my_target) {
+      previous_ptr = previous;
+      return current;
+    }
+    previous = current;
+    current = current->getNext();
   }
   return nullptr;
 }
--- PlayList.h
+++ PlayList.h
@@ -1,30 +1,33 @@
-// #ifndef PLAY_LIST_H_
-// #define PLAY_LIST_H_
+// Title: PlayList.h
+// Author: Josh Winton
+// Purpopse: Function prototypes for playlist class
+// Created: 10/17/18
 
 #include "LinkedSet.h"
 #include "Song.h"
 
 #pragma once
 
-class PlayList : public LinkedSet<Song>{
+class PlayList : public LinkedSet<Song> {
 public:
-  PlayList(); // default constructor
-  PlayList(const Song& a__song); // paramaterized constructor
-  PlayList(const PlayList& a_play_list):LinkedSet(a_play_list){ // copy constructor
+  PlayList();                    // default constructor
+  PlayList(const Song &a__song); // paramaterized constructor
+  PlayList(const PlayList &a_play_list)
+      : LinkedSet(a_play_list) { // copy constructor
     head_ptr_ = a_play_list.head_ptr_;
     tail_ptr_ = a_play_list.getPointerToLastNode();
   }
-  ~PlayList(); // destructor
-  bool add(const Song& new_song) override; // adds song to end of playlist
-  bool remove(const Song& a_song) override; // overridden remove preserves order of songs
-  void loop(); // looping the chain
-  void unloop(); // unlooping the chain
+  ~PlayList();                             // destructor
+  bool add(const Song &new_song) override; // adds song to end of playlist
+  bool remove(const Song &a_song)
+      override;           // overridden remove preserves order of songs
+  void loop();            // looping the chain
+  void unloop();          // unlooping the chain
   void displayPlayList(); // uses toVector to print playlist
 private:
-  Node<Song>* tail_ptr_; // Pointer to last node
-  Node<Song>* getPointerToLastNode() const; // returns pointer to last node
+  Node<Song> *tail_ptr_;                    // Pointer to last node
+  Node<Song> *getPointerToLastNode() const; // returns pointer to last node
   //
-  Node<Song>* getPointerTo(const Song& target, Node<Song>*& previous_ptr) const; // gets pointer to a node; used by remove
+  Node<Song> *getPointerTo(const Song &target, Node<Song> *&previous_ptr)
+      const; // gets pointer to a node; used by remove
 };
-// #include "PlayList.cpp"
-// #endif
